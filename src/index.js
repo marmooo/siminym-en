b30b06d831990a22a805500d0641e7e66b097a35
marmooo <marmooo@users.noreply.github.com>
2023-06-20 11:13:47 +0900

    improve loading icon

--- src/index.js
+++ src/index.js
@@ -76,9 +76,12 @@ async function loadDBWorker(n) {
 }
 
 async function loadDBWorkers() {
+  const loading = document.getElementById("loading");
+  loading.classList.remove("d-none");
   const sizes = [1000, 3000, 10000, 30000];
   const promises = sizes.map(loadDBWorker);
   await Promise.all(promises);
+  loading.classList.add("d-none");
 }
 
 const dbWorkers = {};
--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = "2023-06-20 10:00";
+var CACHE_NAME = "2023-06-20 11:15";
 var urlsToCache = [
   "/siminym-en/",
   "/siminym-en/index.js",
